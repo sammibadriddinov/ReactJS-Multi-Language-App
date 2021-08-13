6313e53d2ed3306f6b191902b8df6e8a2f1b28dc
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-08-13 12:29:01 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, {StrictMode, Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import i18n from 'i18next'
+import {useTranslation, initReactI18next} from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import 'bootstrap/dist/js/bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'flag-icon-css/css/flag-icon.min.css'
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en', 'fr', 'ar', 'uz'],
+    fallbackLng: "en",
+    detection: {
+      order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
+      caches: ['cookie']
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react: {useSuspense: false}
+  });
+
+const loading = (
+  <div className="spinner-border" role="status">
+    <span className="visually-hidden">Loading...</span>
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={loading}>
+    <StrictMode>
+      <App/>
+    </StrictMode>
+  </Suspense>,
   document.getElementById('root')
 );
